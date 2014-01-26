5c70ccdfed42ff7430b7e2971905d61c312c7281
Chris Neale <cin210@nyu.edu>
2014-01-26 12:57:12 -0800

    Upgrade to Rails 4, Ruby 2, Add Devise and basic front end gems

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+gem 'rails', '4.0.2'
+gem 'railties'
+
+
+# DB
+gem 'mysql2'
+
+# Server
+gem 'thin'
+
+# Front-end
+gem 'sass-rails'
+gem 'haml'
+gem 'coffee-rails'
+
+
+gem 'uglifier', '>= 1.0.3'
+gem 'better_errors'
+
+# Javascript
+gem 'jquery-rails'
+
+# Account management
+gem 'devise'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recruiter::Application.config.secret_token = '644a9854c7d2fb61cb7efb3437c6399c4a862b3be106328ca84e3a80db589bd4251c9ebd03cad805c9598b6ad13ccab902d8423382263a8846210bb9e6e5e3bd'

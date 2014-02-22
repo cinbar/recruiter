d92cef97e4491fea499462d17e3d29910b63aece
Chris Neale <cin210@nyu.edu>
2014-02-22 15:01:39 -0800

    Wrap create action in rescue block

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,11 +5,17 @@ class JobsController < ApplicationController
   
   def create
     @job = Job.new
-    @job.title = params[:jobs][:title]
-    @job.source_url = params[:jobs][:source_url]
-    @job.source_id = params[:jobs][:source_id]
+    @job.title         = params[:jobs][:title]
+    @job.source_url    = params[:jobs][:source_url]
+    @job.source_id     = params[:jobs][:source_id]
     @job.source_domain = params[:jobs][:source_domain]
-    @job.json = params[:jobs][:json]
-    @job.save!
+    @job.json          = params[:jobs][:json]
+    begin
+      @job.save!
+      render :nothing, status: 200
+    rescue Exception => e
+      Rails.logger.error("Failed to create job. #{e.message}")
+      render :nothing, status: 500
+    end
   end
 end
